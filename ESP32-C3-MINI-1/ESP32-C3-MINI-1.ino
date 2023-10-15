da7a2e9103faef12cb41501f31f4164301096567
Moritz <22204103+momower1@users.noreply.github.com>
2023-10-15 18:50:15 +0200

    Added RFID-522 sensor reading

--- ESP32-C3-MINI-1/ESP32-C3-MINI-1.ino
+++ ESP32-C3-MINI-1/ESP32-C3-MINI-1.ino
@@ -1,11 +1,21 @@
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 // https://www.uuidgenerator.net/
 // Choose unique identifiers for the BLE service and characteristic
 const char* uuidService = "5f804f25-4bd9-457a-ac2d-ba39563d9b66";
 const char* uuidCharacteristic = "bbe3aeba-fe89-464f-9a3b-b845b758b239";
+
+const byte rfidPinSDA = 5;
+const byte rfidPinRST = 9;
+
+const byte spiPinSCK = 6;
+const byte spiPinMISO = 2;
+const byte spiPinMOSI = 7;
+const byte spiPinSS = rfidPinSDA;
 
 BLEServer* bleServer = nullptr;
 BLECharacteristic* bleCharacteristic = nullptr;
@@ -31,9 +41,15 @@ class ServerCallbacks: public BLEServerCallbacks
 BLE2902 ble2902;
 ServerCallbacks serverCallbacks;
 
+MFRC522 rfid(rfidPinSDA, rfidPinRST);
+
 void setup()
 {
   Serial.begin(115200);
+
+  // RFID-RC522
+  SPI.begin(spiPinSCK, spiPinMISO, spiPinMOSI, spiPinSS);
+  rfid.PCD_Init();
 
   BLEDevice::init("ESP32-C3-MINI-1");
 
@@ -63,6 +79,32 @@ void setup()
 
 void loop()
 {
+  if (rfid.PICC_IsNewCardPresent())
+  {
+    Serial.println("New card present");
+
+    if (rfid.PICC_ReadCardSerial())
+    {
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      
+      for (int i = 0; i < rfid.uid.size; i++)
+      {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+
+      Serial.println();
+
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+    }
+  }
+
   if (bleServer->getConnectedCount() > 0)
   {
     bleCharacteristic->setValue((uint8_t*)&value, sizeof(value));
